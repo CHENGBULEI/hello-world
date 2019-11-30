a38cea68fb2d7eff9b6499b6a3dac64b3c97ca17
CHENGBULEI <17362992091m@sina.cn>
2019-12-01 01:59:33 +0800

    1131_luogu

---
+++ p1071.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	map<char , char>res;
+	string yuanwen;
+	string yiwen;
+	string fanyi;
+	string jieguo;
+	
+	cin>>yuanwen;
+	cin>>yiwen;
+	cin>>fanyi;
+	
+	
+	return 0;
+} 
